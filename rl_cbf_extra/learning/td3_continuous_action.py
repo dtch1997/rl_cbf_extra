134f6113292d5f6cd1073d6df04cffed12ef88a3
Daniel Tan <dtch1997@users.noreply.github.com>
2023-08-08 13:12:51 +0100

    Add safety-constrained training script

--- rl_cbf_extra/learning/td3_continuous_action.py
+++ rl_cbf_extra/learning/td3_continuous_action.py
@@ -90,10 +90,14 @@ class SafetyWalker2dEnv(SafetyEnv):
         # TODO: implement this
         return new_state
 
-
 def parse_args():
-    # fmt: off
     parser = argparse.ArgumentParser()
+    parser = add_args(parser)
+    args = parser.parse_args()
+    return args
+
+def add_args(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
+    # fmt: off   
     parser.add_argument("--exp-name", type=str, default=os.path.basename(__file__).rstrip(".py"),
         help="the name of this experiment")
     parser.add_argument("--seed", type=int, default=1,
@@ -143,9 +147,7 @@ def parse_args():
         help="if toggled, the CBF is bounded")
     parser.add_argument("--supervised", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True,
         help="if toggled, the CBF is supervised")
-    args = parser.parse_args()
-    # fmt: on
-    return args
+    return parser
 
 
 def make_env(env_id, seed, idx, capture_video, run_name):
@@ -229,228 +231,3 @@ class Actor(nn.Module):
         x = torch.from_numpy(x).to(torch.float32).to(device)
         x = x.view(1, -1)
         return self.forward(x).cpu().detach().numpy()[0]
-
-
-if __name__ == "__main__":
-    args = parse_args()
-    run_name = f"{args.env_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
-    if args.track:
-        import wandb
-
-        wandb.init(
-            project=args.wandb_project_name,
-            group=args.wandb_group,
-            entity=args.wandb_entity,
-            sync_tensorboard=True,
-            config=vars(args),
-            name=run_name,
-            monitor_gym=True,
-            save_code=True,
-        )
-    writer = SummaryWriter(f"runs/{run_name}")
-    writer.add_text(
-        "hyperparameters",
-        "|param|value|\n|-|-|\n%s"
-        % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
-    )
-
-    # TRY NOT TO MODIFY: seeding
-    random.seed(args.seed)
-    np.random.seed(args.seed)
-    torch.manual_seed(args.seed)
-    torch.backends.cudnn.deterministic = args.torch_deterministic
-
-    device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
-
-    # env setup
-    envs = gym.vector.SyncVectorEnv(
-        [make_env(args.env_id, args.seed, 0, args.capture_video, run_name)]
-    )
-    # Create dummy env for sampling states
-    _env = gym.make(args.env_id)
-    _env = SafetyWalker2dEnv(_env, override_reward=True)
-    _env = gym.wrappers.TimeLimit(_env, max_episode_steps=1000)
-    assert isinstance(
-        envs.single_action_space, gym.spaces.Box
-    ), "only continuous action space is supported"
-
-    actor = Actor(envs).to(device)
-    qf1 = QNetwork(envs, bounded=args.bounded).to(device)
-    qf2 = QNetwork(envs, bounded=args.bounded).to(device)
-    qf1_target = QNetwork(envs, bounded=args.bounded).to(device)
-    qf2_target = QNetwork(envs, bounded=args.bounded).to(device)
-    target_actor = Actor(envs).to(device)
-    target_actor.load_state_dict(actor.state_dict())
-    qf1_target.load_state_dict(qf1.state_dict())
-    qf2_target.load_state_dict(qf2.state_dict())
-    q_optimizer = optim.Adam(
-        list(qf1.parameters()) + list(qf2.parameters()), lr=args.learning_rate
-    )
-    actor_optimizer = optim.Adam(list(actor.parameters()), lr=args.learning_rate)
-
-    envs.single_observation_space.dtype = np.float32
-    rb = ReplayBuffer(
-        args.buffer_size,
-        envs.single_observation_space,
-        envs.single_action_space,
-        device,
-        handle_timeout_termination=True,
-    )
-    start_time = time.time()
-
-    # TRY NOT TO MODIFY: start the game
-    obs = envs.reset()
-    for global_step in range(args.total_timesteps):
-        # ALGO LOGIC: put action logic here
-        if global_step < args.learning_starts:
-            actions = np.array(
-                [envs.single_action_space.sample() for _ in range(envs.num_envs)]
-            )
-        else:
-            with torch.no_grad():
-                actions = actor(torch.Tensor(obs).to(device))
-                actions += torch.normal(0, actor.action_scale * args.exploration_noise)
-                actions = (
-                    actions.cpu()
-                    .numpy()
-                    .clip(envs.single_action_space.low, envs.single_action_space.high)
-                )
-
-        # TRY NOT TO MODIFY: execute the game and log data.
-        next_obs, rewards, dones, infos = envs.step(actions)
-
-        # TRY NOT TO MODIFY: record rewards for plotting purposes
-        for info in infos:
-            if "episode" in info.keys():
-                print(
-                    f"global_step={global_step}, episodic_return={info['episode']['r']}"
-                )
-                writer.add_scalar(
-                    "charts/episodic_return", info["episode"]["r"], global_step
-                )
-                writer.add_scalar(
-                    "charts/episodic_length", info["episode"]["l"], global_step
-                )
-                break
-
-        # TRY NOT TO MODIFY: save data to reply buffer; handle `terminal_observation`
-        real_next_obs = next_obs.copy()
-        for idx, d in enumerate(dones):
-            if d:
-                real_next_obs[idx] = infos[idx]["terminal_observation"]
-        rb.add(obs, real_next_obs, actions, rewards, dones, infos)
-
-        # TRY NOT TO MODIFY: CRUCIAL step easy to overlook
-        obs = next_obs
-
-        # ALGO LOGIC: training.
-        if global_step > args.learning_starts:
-            data = rb.sample(args.batch_size)
-            with torch.no_grad():
-                clipped_noise = (
-                    torch.randn_like(data.actions, device=device) * args.policy_noise
-                ).clamp(-args.noise_clip, args.noise_clip) * target_actor.action_scale
-
-                next_state_actions = (
-                    target_actor(data.next_observations) + clipped_noise
-                ).clamp(
-                    envs.single_action_space.low[0], envs.single_action_space.high[0]
-                )
-                qf1_next_target = qf1_target(data.next_observations, next_state_actions)
-                qf2_next_target = qf2_target(data.next_observations, next_state_actions)
-                min_qf_next_target = torch.min(qf1_next_target, qf2_next_target)
-                next_q_value = data.rewards.flatten() + (
-                    1 - data.dones.flatten()
-                ) * args.gamma * (min_qf_next_target).view(-1)
-
-            qf1_a_values = qf1(data.observations, data.actions).view(-1)
-            qf2_a_values = qf2(data.observations, data.actions).view(-1)
-            qf1_loss = F.mse_loss(qf1_a_values, next_q_value)
-            qf2_loss = F.mse_loss(qf2_a_values, next_q_value)
-            qf_loss = qf1_loss + qf2_loss
-
-            # optimize the model
-            q_optimizer.zero_grad()
-            qf_loss.backward()
-            q_optimizer.step()
-
-            if args.supervised:
-                # Apply supervised loss
-                states = _env.sample_states(args.batch_size)
-                is_unsafe = _env.is_unsafe(states).squeeze()
-                unsafe_states = states[is_unsafe == 1].astype(np.float32)
-                unsafe_states = (
-                    torch.from_numpy(unsafe_states).to(device).to(torch.float32)
-                )
-                actions = actor(unsafe_states)
-                unsafe_qpred_1 = qf1(unsafe_states, actions)
-                unsafe_qpred_2 = qf2(unsafe_states, actions)
-                unsafe_val_pred = torch.min(unsafe_qpred_1, unsafe_qpred_2)
-
-                unsafe_val_true = 0 * torch.ones(
-                    unsafe_val_pred.shape, device=device, dtype=torch.float32
-                )
-                unsafe_loss = F.mse_loss(unsafe_val_pred, unsafe_val_true)
-                q_optimizer.zero_grad()
-                unsafe_loss.backward()
-                q_optimizer.step()
-
-            if global_step % args.policy_frequency == 0:
-                actor_loss = -qf1(data.observations, actor(data.observations)).mean()
-                actor_optimizer.zero_grad()
-                actor_loss.backward()
-                actor_optimizer.step()
-
-                # update the target network
-                for param, target_param in zip(
-                    actor.parameters(), target_actor.parameters()
-                ):
-                    target_param.data.copy_(
-                        args.tau * param.data + (1 - args.tau) * target_param.data
-                    )
-                for param, target_param in zip(
-                    qf1.parameters(), qf1_target.parameters()
-                ):
-                    target_param.data.copy_(
-                        args.tau * param.data + (1 - args.tau) * target_param.data
-                    )
-                for param, target_param in zip(
-                    qf2.parameters(), qf2_target.parameters()
-                ):
-                    target_param.data.copy_(
-                        args.tau * param.data + (1 - args.tau) * target_param.data
-                    )
-
-            if global_step % 100 == 0:
-                writer.add_scalar(
-                    "losses/qf1_values", qf1_a_values.mean().item(), global_step
-                )
-                writer.add_scalar(
-                    "losses/qf2_values", qf2_a_values.mean().item(), global_step
-                )
-                writer.add_scalar("losses/qf1_loss", qf1_loss.item(), global_step)
-                writer.add_scalar("losses/qf2_loss", qf2_loss.item(), global_step)
-                writer.add_scalar("losses/qf_loss", qf_loss.item() / 2.0, global_step)
-                writer.add_scalar("losses/actor_loss", actor_loss.item(), global_step)
-                if args.supervised:
-                    writer.add_scalar("losses/unsafe_loss", unsafe_loss, global_step)
-                print("SPS:", int(global_step / (time.time() - start_time)))
-                writer.add_scalar(
-                    "charts/SPS",
-                    int(global_step / (time.time() - start_time)),
-                    global_step,
-                )
-
-        if args.track:
-            # make sure to tune `CHECKPOINT_FREQUENCY`
-            # so models are not saved too frequently
-            if global_step % 10000 == 0:
-                torch.save(actor.state_dict(), f"{wandb.run.dir}/actor.pt")
-                torch.save(qf1.state_dict(), f"{wandb.run.dir}/qf1.pt")
-                torch.save(qf2.state_dict(), f"{wandb.run.dir}/qf2.pt")
-                wandb.save(f"{wandb.run.dir}/actor.pt")
-                wandb.save(f"{wandb.run.dir}/qf1.pt")
-                wandb.save(f"{wandb.run.dir}/qf2.pt")
-
-    envs.close()
-    writer.close()
---
+++ rl_cbf_extra/learning/td3_continuous_action_train_constrain.py
@@ -0,0 +1,310 @@
+from rl_cbf_extra.learning.td3_continuous_action import *
+
+
+def load_constraint(save_dir, args):
+    """Load constraint from save_dir"""
+    envs = gym.vector.SyncVectorEnv(
+        [make_env(args.env_id, args.seed, 0, args.capture_video, "eval")]
+    )
+    actor = Actor(envs)
+    qf1 = QNetwork(envs, bounded=args.bounded)
+    qf2 = QNetwork(envs, bounded=args.bounded)
+
+    qf1.load_state_dict(torch.load(f"{save_dir}/qf1.pt"))
+    qf2.load_state_dict(torch.load(f"{save_dir}/qf2.pt"))
+    actor.load_state_dict(torch.load(f"{save_dir}/actor.pt"))
+    safety_threshold = 0.5 / (1 - args.gamma)
+    return actor, qf1, qf2, safety_threshold
+
+
+def get_constrained_action(
+    actor: torch.nn.Module,
+    qf1: torch.nn.Module,
+    qf2: torch.nn.Module,
+    safety_threshold: float,
+    state: np.ndarray,
+    action: np.ndarray,
+):
+    """Return action that satisfies the safety constraint"""
+    next_qf1_value = qf1.predict(state, action).item()
+    next_qf2_value = qf2.predict(state, action).item()
+    next_barrier_value = min(next_qf1_value, next_qf2_value)
+
+    if next_barrier_value < safety_threshold:
+        # Choose the action with the highest Q-value
+        action = actor.predict(state)
+    return action
+
+
+def make_env(env_id, seed, idx, capture_video, run_name):
+    def thunk():
+        env = gym.make(env_id)
+        env = gym.wrappers.RecordEpisodeStatistics(env)
+        if capture_video:
+            if idx == 0:
+                env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
+        env.seed(seed)
+        env.action_space.seed(seed)
+        env.observation_space.seed(seed)
+        return env
+
+    return thunk
+
+
+def add_constrain_train_args(
+    parser: argparse.ArgumentParser,
+) -> argparse.ArgumentParser:
+    # TODO: implement
+    parser.add_argument("--constraint_dir", type=str, default="downloads/1qxv7iyl")
+    parser.add_argument(
+        "--constrain",
+        type=lambda x: bool(strtobool(x)),
+        default=False,
+        nargs="?",
+        const=True,
+    )
+    return parser
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser = add_args(parser)
+    parser = add_constrain_train_args(parser)
+    args = parser.parse_args()
+
+    constraint = load_constraint(args.constraint_dir, args)
+
+    run_name = f"{args.env_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
+    if args.track:
+        import wandb
+
+        wandb.init(
+            project=args.wandb_project_name,
+            group=args.wandb_group,
+            entity=args.wandb_entity,
+            sync_tensorboard=True,
+            config=vars(args),
+            name=run_name,
+            monitor_gym=True,
+            save_code=True,
+        )
+    writer = SummaryWriter(f"runs/{run_name}")
+    writer.add_text(
+        "hyperparameters",
+        "|param|value|\n|-|-|\n%s"
+        % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
+    )
+
+    # TRY NOT TO MODIFY: seeding
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.backends.cudnn.deterministic = args.torch_deterministic
+
+    device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
+
+    # env setup
+    envs = gym.vector.SyncVectorEnv(
+        [make_env(args.env_id, args.seed, 0, args.capture_video, run_name)]
+    )
+    # Create dummy env for sampling states
+    _env = gym.make(args.env_id)
+    assert isinstance(
+        envs.single_action_space, gym.spaces.Box
+    ), "only continuous action space is supported"
+
+    actor = Actor(envs).to(device)
+    qf1 = QNetwork(envs, bounded=args.bounded).to(device)
+    qf2 = QNetwork(envs, bounded=args.bounded).to(device)
+    qf1_target = QNetwork(envs, bounded=args.bounded).to(device)
+    qf2_target = QNetwork(envs, bounded=args.bounded).to(device)
+    target_actor = Actor(envs).to(device)
+    target_actor.load_state_dict(actor.state_dict())
+    qf1_target.load_state_dict(qf1.state_dict())
+    qf2_target.load_state_dict(qf2.state_dict())
+    q_optimizer = optim.Adam(
+        list(qf1.parameters()) + list(qf2.parameters()), lr=args.learning_rate
+    )
+    actor_optimizer = optim.Adam(list(actor.parameters()), lr=args.learning_rate)
+
+    envs.single_observation_space.dtype = np.float32
+    rb = ReplayBuffer(
+        args.buffer_size,
+        envs.single_observation_space,
+        envs.single_action_space,
+        device,
+        handle_timeout_termination=True,
+    )
+    start_time = time.time()
+
+    safety_violations = 0
+
+    # TRY NOT TO MODIFY: start the game
+    obs = envs.reset()
+    for global_step in range(args.total_timesteps):
+        # ALGO LOGIC: put action logic here
+        if global_step < args.learning_starts:
+            actions = np.array(
+                [envs.single_action_space.sample() for _ in range(envs.num_envs)]
+            )
+        else:
+            with torch.no_grad():
+                actions = actor(torch.Tensor(obs).to(device))
+                actions += torch.normal(0, actor.action_scale * args.exploration_noise)
+                actions = (
+                    actions.cpu()
+                    .numpy()
+                    .clip(envs.single_action_space.low, envs.single_action_space.high)
+                )
+        if args.constrain:
+            actions = np.array(
+                [get_constrained_action(*constraint, obs[0], actions[0])]
+            )
+
+        # TRY NOT TO MODIFY: execute the game and log data.
+        next_obs, rewards, dones, infos = envs.step(actions)
+
+        # TRY NOT TO MODIFY: record rewards for plotting purposes
+        for info in infos:
+            if "episode" in info.keys():
+                print(
+                    f"global_step={global_step}, episodic_return={info['episode']['r']}"
+                )
+                writer.add_scalar(
+                    "charts/episodic_return", info["episode"]["r"], global_step
+                )
+                writer.add_scalar(
+                    "charts/episodic_length", info["episode"]["l"], global_step
+                )
+                break
+
+        # Record safety violations
+        for idx, d in enumerate(dones):
+            if d:
+                safety_violations += 1
+
+        # TRY NOT TO MODIFY: save data to reply buffer; handle `terminal_observation`
+        real_next_obs = next_obs.copy()
+        for idx, d in enumerate(dones):
+            if d:
+                real_next_obs[idx] = infos[idx]["terminal_observation"]
+        rb.add(obs, real_next_obs, actions, rewards, dones, infos)
+
+        # TRY NOT TO MODIFY: CRUCIAL step easy to overlook
+        obs = next_obs
+
+        # ALGO LOGIC: training.
+        if global_step > args.learning_starts:
+            data = rb.sample(args.batch_size)
+            with torch.no_grad():
+                clipped_noise = (
+                    torch.randn_like(data.actions, device=device) * args.policy_noise
+                ).clamp(-args.noise_clip, args.noise_clip) * target_actor.action_scale
+
+                next_state_actions = (
+                    target_actor(data.next_observations) + clipped_noise
+                ).clamp(
+                    envs.single_action_space.low[0], envs.single_action_space.high[0]
+                )
+                qf1_next_target = qf1_target(data.next_observations, next_state_actions)
+                qf2_next_target = qf2_target(data.next_observations, next_state_actions)
+                min_qf_next_target = torch.min(qf1_next_target, qf2_next_target)
+                next_q_value = data.rewards.flatten() + (
+                    1 - data.dones.flatten()
+                ) * args.gamma * (min_qf_next_target).view(-1)
+
+            qf1_a_values = qf1(data.observations, data.actions).view(-1)
+            qf2_a_values = qf2(data.observations, data.actions).view(-1)
+            qf1_loss = F.mse_loss(qf1_a_values, next_q_value)
+            qf2_loss = F.mse_loss(qf2_a_values, next_q_value)
+            qf_loss = qf1_loss + qf2_loss
+
+            # optimize the model
+            q_optimizer.zero_grad()
+            qf_loss.backward()
+            q_optimizer.step()
+
+            if args.supervised:
+                # Apply supervised loss
+                states = _env.sample_states(args.batch_size)
+                is_unsafe = _env.is_unsafe(states).squeeze()
+                unsafe_states = states[is_unsafe == 1].astype(np.float32)
+                unsafe_states = (
+                    torch.from_numpy(unsafe_states).to(device).to(torch.float32)
+                )
+                actions = actor(unsafe_states)
+                unsafe_qpred_1 = qf1(unsafe_states, actions)
+                unsafe_qpred_2 = qf2(unsafe_states, actions)
+                unsafe_val_pred = torch.min(unsafe_qpred_1, unsafe_qpred_2)
+
+                unsafe_val_true = 0 * torch.ones(
+                    unsafe_val_pred.shape, device=device, dtype=torch.float32
+                )
+                unsafe_loss = F.mse_loss(unsafe_val_pred, unsafe_val_true)
+                q_optimizer.zero_grad()
+                unsafe_loss.backward()
+                q_optimizer.step()
+
+            if global_step % args.policy_frequency == 0:
+                actor_loss = -qf1(data.observations, actor(data.observations)).mean()
+                actor_optimizer.zero_grad()
+                actor_loss.backward()
+                actor_optimizer.step()
+
+                # update the target network
+                for param, target_param in zip(
+                    actor.parameters(), target_actor.parameters()
+                ):
+                    target_param.data.copy_(
+                        args.tau * param.data + (1 - args.tau) * target_param.data
+                    )
+                for param, target_param in zip(
+                    qf1.parameters(), qf1_target.parameters()
+                ):
+                    target_param.data.copy_(
+                        args.tau * param.data + (1 - args.tau) * target_param.data
+                    )
+                for param, target_param in zip(
+                    qf2.parameters(), qf2_target.parameters()
+                ):
+                    target_param.data.copy_(
+                        args.tau * param.data + (1 - args.tau) * target_param.data
+                    )
+
+            if global_step % 100 == 0:
+                writer.add_scalar(
+                    "losses/qf1_values", qf1_a_values.mean().item(), global_step
+                )
+                writer.add_scalar(
+                    "losses/qf2_values", qf2_a_values.mean().item(), global_step
+                )
+                writer.add_scalar("losses/qf1_loss", qf1_loss.item(), global_step)
+                writer.add_scalar("losses/qf2_loss", qf2_loss.item(), global_step)
+                writer.add_scalar("losses/qf_loss", qf_loss.item() / 2.0, global_step)
+                writer.add_scalar("losses/actor_loss", actor_loss.item(), global_step)
+                if args.supervised:
+                    writer.add_scalar("losses/unsafe_loss", unsafe_loss, global_step)
+                print("SPS:", int(global_step / (time.time() - start_time)))
+                writer.add_scalar(
+                    "charts/SPS",
+                    int(global_step / (time.time() - start_time)),
+                    global_step,
+                )
+                writer.add_scalar(
+                    "charts/safety_violations", safety_violations, global_step
+                )
+
+        if args.track:
+            # make sure to tune `CHECKPOINT_FREQUENCY`
+            # so models are not saved too frequently
+            if global_step % 10000 == 0:
+                torch.save(actor.state_dict(), f"{wandb.run.dir}/actor.pt")
+                torch.save(qf1.state_dict(), f"{wandb.run.dir}/qf1.pt")
+                torch.save(qf2.state_dict(), f"{wandb.run.dir}/qf2.pt")
+                wandb.save(f"{wandb.run.dir}/actor.pt")
+                wandb.save(f"{wandb.run.dir}/qf1.pt")
+                wandb.save(f"{wandb.run.dir}/qf2.pt")
+
+    envs.close()
+    writer.close()
